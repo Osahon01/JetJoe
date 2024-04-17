797abf549ceee205461438bb15c49c1695041220
Osahon <osahonodiase1234@gmail.com>
2024-04-17 18:27:14 -0400

    updated

--- Part1.py
+++ Part1.py
@@ -27,16 +27,8 @@ eq5 = Eq(Ws_turb, OmegaR1*C1*np.sin(a1))
 
 solution = solve((eq1, eq2, eq3, eq4, eq5), (W1, C1, C_theta1, C_theta2, Ws_turb))
 
-def TurbineWork_glob():
-    global W_Turb
-    def W_Turb(): 
-        return solution[Ws_turb]
-
-TurbineWork_glob()
-
-# Call `function()` from outside create_global_function()
-result = W_Turb()
-print("Result from outside:", result)
+def W_Turb(): 
+    return solution[Ws_turb]
 
 # Print the solution
 print(solution)
