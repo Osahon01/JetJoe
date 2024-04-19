0823dde723e5357814026b04cccf0ab95044d3dd
Conrad Casebolt <conradmc@mit.edu>
2024-04-19 17:01:37 -0400

    Streamlined code

--- Part2.py
+++ Part2.py
@@ -13,25 +13,6 @@ h_f = 42 * 10**6 # [J/kg]  (Kerosine)
 gamma_C = 1.4
 gamma_T = 1.3
 
-# Equations
-eq1 = Eq((1+f) * c_pT * (T_t4-T_t5), c_pC * (T_t3-T_t2))
-eq2 = Eq((1+f) * c_pT * T_t4, f*h_f + c_pC*T_t3)
-eq3 = Eq(shaft_pow/c_pT, T_t4-T_t5)
-# Proposed different eq3 using compressor should yield same result
-eq3_prime = Eq(shaft_pow/c_pC, T_t3-T_t2)
-
-#Solve
-solution = solve((eq1, eq2, eq3), (f, T_t3, T_t4))
-print(solution)
-f = solution[0][0]
-T_t3 = solution[0][1]
-T_t4 = solution[0][2]
-# Solution for eq3 prime using compressor specific work instead
-# solution_prime = solve((eq1, eq2, eq3_prime), (f, T_t3, T_t4))
-# print("\n\n Alternatively\n")
-# print(solution_prime)
-# Why is it slightly different?
-
 print("\n\n Analytical solution")
 # Analytical Solution for step 3 (solved by hand)
 f = (-1*(c_pT*T_t5+h_f)+np.sqrt((c_pT*T_t5+h_f)**2 - 4*h_f*(T_t2*c_pC-c_pT*T_t5)))/(2*h_f)
@@ -53,22 +34,21 @@ print("T_t4 Analytical = ", T_t4, " K")
 # Knowns
 R = 287
 eta_T = 0.6 # WE SET THIS
-A_T = 0.00258064 # [m^2] (measured) (DUMMY VALUE 4 in^2)
 P_0 = 101325 # [Pa] in atmosphere
 P_t0 = P_0
 P_t2 = P_t0
 P_t3 = comp_ratio * P_0
-P_t4 = P_t3 # assuming constant pressure combustion
+P_t4 = P_t3 # assuming deltaKE is negligible in combustor
 
 # Pressure after turbine as a function of thermal efficiency
 P_t5 = P_t4*((T_t5/T_t4 - 1)/eta_T + 1)**(gamma_T/(gamma_T-1))
-T_6 =300
+T_6 = 300
 P6 = 101325
 T_t6 = T_t5
 P_t6 = P_t5
 M6 = ((2/(gamma_T - 1))*((P6/P_t6)**(-1*(gamma_T - 1) / gamma_T) -1))**0.5
 c6 = M6*(gamma_T*287*T_6)**0.5
-print(f'c6 here', c6, M6)
+print("c6 = ", c6, " and M6 = ", M6)
 
 # Solve for mass flow after the turbine (but its the same everywhere)
 A_NGV = 0.000698744409 # m^2
@@ -80,17 +60,10 @@ KE = (0.5*m*c6**2) #STEP 6 --> verify on piazza
 print(f'Mass flow = {m} [kg/s] ; KE = {KE} []')
 
 #Step 7 
+cfact = (36004.4448*(1/0.4536))
 T0 = 300
 TSFC = (f / (c6*(f+1)))
+TSFC_new = TSFC*cfact
 T_spec = (f+1)*(c6 / ((gamma_C*287*T0)**0.5) )
 print(f'TSFC = {TSFC} ; Specific Thrust = {T_spec}')
-
-# # STEP 6
-# A_6 = 0.0013283844 # [m^2] measured; converted from 2.059 in^2
-# A_4 = 0.001 # [m^2] measured (DUMMY VALUE TIL WE CALCULATE)
-# # Solve for Mach number at nozzle exit
-# M_6 = symbols('M_6')
-# f_M = Eq(A_6/A_4, 1/M_6*(2/(gamma_T+1)*(1 + (gamma_T-1)/2 * M_6**2))**(1/2 * (gamma_T-1)/(gamma_T+1)))
-# solution = solve((f_M), (M_6))
-# print(solution)
-# print("TEst")
+print(f'TSFC = {TSFC_new}')
