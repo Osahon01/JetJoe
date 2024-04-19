29f7bbe234257e8e899e62e35bc867dab09778a5
Conrad Casebolt <conradmc@mit.edu>
2024-04-19 08:08:58 -0400

    deprecated

--- Part2.py
+++ Part2.py
@@ -9,6 +9,7 @@ T_t5 = 953.15 # 680 + 273.15
 c_pC = 1004.5
 c_pT = 1243.67
 shaft_pow = W_Turb() #From Part 1
+print("SHAFT POWER ", shaft_pow, " J/kg")
 h_f = 42 * 10**6 # [J/kg]  (Kerosine)
 gamma_C = 1.4
 gamma_T = 1.3
@@ -40,7 +41,7 @@ print("f Analytical = ", f)
 print("T_t3 Analytical = ", T_t3, " K")
 
 # Solving for Compressor pressure ratio
-eta_C = 0.1 # WE SET THIS (burner value for now)
+eta_C = 0.5 # WE SET THIS (burner value for now)
 comp_ratio = (eta_C*(T_t3/T_t2 - 1) + 1)**(gamma_C/(gamma_C-1))
 print("Compression Ratio = ", comp_ratio, " assuming eta_C = ", eta_C)
 
@@ -52,34 +53,39 @@ print("T_t4 Analytical = ", T_t4, " K")
 
 # Knowns
 R = 287
-eta_T = 0.8 # WE SET THIS
-A_T = 0.00258064 # [m^2] (measured) (DUMMY VALUE 4 in^2)
+# eta_T = 0.8 # WE SET THIS
+# A_T = 0.00258064 # [m^2] (measured) (DUMMY VALUE 4 in^2)
+A_NGV = 0.000698744409 # [m^2] measured
 P_0 = 101325 # [Pa] in atmosphere
 P_t0 = P_0
 P_t2 = P_t0
 P_t3 = comp_ratio * P_0
 P_t4 = P_t3 # assuming constant pressure combustion
 
-# Pressure after turbine as a function of thermal efficiency
-P_t5 = P_t4*((T_t5/T_t4 - 1)/eta_T + 1)**(gamma_T/(gamma_T-1))
-P6 = 101325
-T_t6 = T_t5
-P_t6 = P_t5
-M6 = ((2/(gamma_T - 1))*((P6/P_t6)**((gamma_T - 1) / gamma_T) -1))**0.5
-c6 = M6*(gamma_T*287*T_t6)
-print('here', c6, M6)
-
-# Solve for mass flow after the turbine (but its the same everywhere)
+# Solve for mass flow after the NGV (but its the same everywhere)
 M_4 = 1 # for choked NGV
 DM_4 = M_4/(1 + ((gamma_T-1)/2)*M_4**2)**(1/2 * (gamma_T+1)/(gamma_T-1)) # corrected mass flow per unit area
-DM_5 = DM_4 # conserved along the turbine (I believe)
-m = (DM_5 * A_T * P_t5 * (gamma_T)**(1/2))/(R*T_t5)**(1/2)
+m = (DM_4 * A_NGV * P_t4 * (gamma_T)**(1/2))/(R*T_t4)**(1/2)
+# DM_5 = DM_4 # conserved along the turbine (I believe)
+# m = (DM_5 * A_T * P_t5 * (gamma_T)**(1/2))/(R*T_t5)**(1/2)
 print("Mass flow = ", m, " kg/s")
 
-# # STEP 6
+# STEP 6
+# Knowns 
+eta_T = 0.5 # WE SET THIS
+P_6 = P_0
+T_6 = 300
+# Pressure after turbine as a function of thermal efficiency
+P_t5 = P_t4*((T_t5/T_t4 - 1)/eta_T + 1)**(gamma_T/(gamma_T-1))
+P_t6 = P_t5
+M_6 = (2/(gamma_T-1)*(P_t6/P_6)**((gamma_T-1)/gamma_T))**(1/2)
+a_6 = (gamma_T*R*T_6)**(1/2)
+c_6 = M_6 * a_6
+print("Mach number at nozzle exit = ", M_6)
+print("Nozzle exit velocity = ", c_6)
 # A_6 = 0.0013283844 # [m^2] measured; converted from 2.059 in^2
 # A_4 = 0.001 # [m^2] measured (DUMMY VALUE TIL WE CALCULATE)
-# # Solve for Mach number at nozzle exit
+# Solve for Mach number at nozzle exit
 # M_6 = symbols('M_6')
 # f_M = Eq(A_6/A_4, 1/M_6*(2/(gamma_T+1)*(1 + (gamma_T-1)/2 * M_6**2))**(1/2 * (gamma_T-1)/(gamma_T+1)))
 # solution = solve((f_M), (M_6))
