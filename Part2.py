e60a8084b7f0bc1ccf4fd7a032a99c072ac5c546
Conrad Casebolt <conradmc@mit.edu>
2024-04-26 21:58:22 -0400

    Final Model Code.  We should not change this

--- Part2.py
+++ Part2.py
@@ -8,14 +8,12 @@ f, T_t3, T_t4 = symbols('f, T_t3, T_t4')
 print("\nStep 1")
 
 # Known variables
-a1 = np.deg2rad(35.26)
-b1 = -np.deg2rad(19)
-# b2 = np.deg2rad(45) # Around Alex's group?
+a1 = np.deg2rad(35.26)# 35.26
+b1 = -np.deg2rad(19) # 19
 Omega = 160000 * (2*np.pi)/60 # Convert from RPM to rad/s
 r1 = 0.02293760598
 OmegaR1 = Omega*r1
 w_T = OmegaR1**2 * 1/(1 - np.tan(b1)/np.tan(a1))
-# w_T = OmegaR1**2 * np.tan(a1)/np.tan(b2)
 print(f'w_T = {w_T} [J/kg]')
 
 # Step 2
@@ -26,6 +24,7 @@ print("We chose eta_C = eta_T = 0.6")
 print("\nStep 3")
 
 # Known Variables
+T_0 = 300
 T_t2 = 300
 T_t5 = 953.15 # 680 + 273.15
 c_pC = 1004.5
@@ -42,7 +41,7 @@ print(f'f = {f}')
 print(f'T_t3  = {T_t3} K')
 
 # Calculating Compressor pressure ratio
-eta_C = 0.60 # WE SET THIS
+eta_C = 0.63 # WE SET THIS
 comp_ratio = (eta_C*(T_t3/T_t2 - 1) + 1)**(gamma_C/(gamma_C-1))
 print(f'Compression Ratio =  {comp_ratio}  assuming eta_C = {eta_C}')
 
@@ -58,8 +57,8 @@ print("\nStep 5")
 
 # Knowns
 R = 287
-eta_T = 0.80 # WE SET THIS
-P_0 = 101325 # [Pa] Atmospheric pressure at sea level
+eta_T = 0.70 # WE SET THIS
+P_0 = 101325 #101325 # [Pa] Atmospheric pressure at sea level
 P_t0 = P_0 # c_0 = 0 (atmosphere is initially at rest wrt inlet)
 P_t2 = P_t0 # Stagnation pressure conserved along streamline up to the compressor
 P_t3 = comp_ratio * P_0 # Centrifugal Compression occurs
@@ -68,7 +67,7 @@ P_t4 = P_t3 # Stagnation pressure conserved across the combustor
 # Pressure after turbine as a function of thermal efficiency
 P_t5 = P_t4*((T_t5/T_t4 - 1)/eta_T + 1)**(gamma_T/(gamma_T-1))
 # P_t5 = P_t4*((T_t5/T_t4))**(gamma_T/(gamma_T-1)) # If eta_T = 1
-T_6 = 300
+T_6 = 300 #300
 P6 = 101325
 T_t6 = T_t5
 P_t6 = P_t5
@@ -76,15 +75,15 @@ M6 = ((2/(gamma_T - 1))*((P_t6/P6)**((gamma_T - 1) / gamma_T) -1))**0.5
 T_exit = T_t6/(1+(gamma_T-1)/2 * M6**2)
 print(f'T_exit = {T_exit} K')
 a6 = (gamma_T * R * T_exit)**0.5
-# c6 = M6*a6
-c6 = (2*c_pT*T_t5*(1-(T_t4/T_t5)*((P_0/P_t3)**((gamma_T - 1)/gamma_T))))**0.5
+c6 = M6*a6
+# T_t5_s = T_t5 + (c_pC*(T_t3-T_0))/(c_pT*(f+1))*(1-1/eta_T)
+# c6 = (2*c_pT*T_t5*(1-(T_t4/T_t5_s)*((P_0/P_t3)**((gamma_T - 1)/gamma_T))))**0.5
 print(f'c6 = {c6} and M6 = {M6} and a6 = {a6}')
 
 # Solve for mass flow after the turbine (but its the same everywhere)
 A_NGV = 0.000698744409 # m^2 measured
 M_4 = 1 # for choked NGV
 DM_4 = M_4/(1 + ((gamma_T-1)/2)*M_4**2)**(1/2 * (gamma_T+1)/(gamma_T-1)) # corrected mass flow per unit area
-# DM_5 = DM_4 # conserved along the turbine (I believe)
 m4 = (DM_4 * A_NGV * P_t4 * (gamma_T)**(1/2))/(R*T_t4)**(1/2)
 print(f'NGV mass flow = {m4} [kg/s]')
 
@@ -96,10 +95,10 @@ print(f'KE_dot = {KE} [W = J/s]')
 # Step 7
 print("\nStep 7")
 cfact = (3600*4.4448*(1/0.4536)) # (kg/s)/N to (lbm/hr)/lbf
-T0 = 300
+T0 = 300 
 TSFC = (f / (c6*(f+1)))
 TSFC_imperial = TSFC*cfact
-T_spec = (f+1)*(c6 / ((gamma_C*287*T0)**0.5) )
+T_spec = (f+1)*(c6 / ((gamma_C*287*T0)**0.5))
 print(f'TSFC = {TSFC} ; Specific Thrust = {T_spec}')
 print(f'TSFC Imperial = {TSFC_imperial}')
 
@@ -110,8 +109,8 @@ print(f'Mass flow at inlet = {m1} [kg/s]')
 # SANITY CHECK
 print("\nCheck if our model is reasonable")
 T = m4*c6
-print(f'Thrust = {T} [N], Thrust Imperial: {T / 4.44822} [lbf]')
-cfact2 = (1/4.482) * (1/60) * 16 # (lbm/N) * (min/hr)^-1 * oz/lbm
+print(f'Thrust = {T} [N], Thrust Imperial: {T / 4.4482} [lbf]')
+cfact2 = (1/4.4482) * (1/60) * 16 # (lbf/N) * (min/hr)^-1 * oz/lbm
 fuel_cons = T * TSFC_imperial * cfact2
 print(f'Fuel consumption = {fuel_cons} [oz/min]')
 
